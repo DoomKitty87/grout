3370ea4e451c5a38e98200bf917bea7f73fb0388
LennaKiser <146594590+LennaKiser@users.noreply.github.com>
2025-11-15 17:00:47 -0800

    I added some stuff

--- expo-router/app/settings/index.tsx
+++ expo-router/app/settings/index.tsx
@@ -1,11 +1,24 @@
-import { Text, View } from 'tamagui'
+import { H3, Slider, Text, View } from 'tamagui'
 
 export default function TabTwoScreen() {
   return (
     <View flex={1} items="center" justify="center" bg="$background">
+      
+      <H3>
+        Prioritization
+      </H3>
+
+      <Slider size="$2" width={200} defaultValue={[50]} max={100} step={1}>
+    <Slider.Track>
+      <Slider.TrackActive />
+    </Slider.Track>
+    <Slider.Thumb circular index={0} />
+     </Slider>
+      
       <Text fontSize={20} color="$blue10">
-        Tab Two
+        Percentage Value
       </Text>
+
     </View>
   )
 }
